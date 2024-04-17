532fa037cb204c27ed4bbce583bc35bb6134505f
Jan H <janheymann2@gmail.com>
2024-04-17 15:15:26 +0200

    delete all the webpack stuff, compile current version

---
+++ generate_lighttpd.conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Locally, I have a lighttpd.conf file, but i'm not putting it into git
+# because the path to this folder is hardcoded into it, and I think 
+# just running this script is easier than actually typing the path into
+# lighttpd.conf. Stupidest shell script i've ever written.
+touch lighttpd.conf &&
+exec > lighttpd.conf &&
+readlink -e $0 | sed -e "s/\(^.*\/\).*/server.document-root = \"\1\"/" &&
+cat /dev/stdin <<Q
+
+server.port = 3000
+
+# If running lighttpd earlier than lighttpd 1.4.71, uncomment (remove '#') to add the following:
+mimetype.assign = (
+  ".wasm" => "application/wasm",
+  ".js"  => "text/javascript",
+  ".html" => "text/html", 
+  ".txt" => "text/plain",
+  ".jpg" => "image/jpeg",
+  ".png" => "image/png" 
+)
+Q
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-import * as wasm from "hello-wasm-pack";
+import init, { run } from "./pkg/wasm_crate.js";
+/* function read_byte() {
+    let el = document.getElementById("input");
+    
+    
+    ret = el.innerText.charAt(0);
+    el.innerText = el.innerText.slice(1);
+    return ret.charCodeAt(0) | 255
+} */
+let addr = new URL(window.location);
+let program = atob(addr.searchParams.get("program"));
+document.getElementById("program").value = program;
+let input = atob(addr.searchParams.get("input"));
+document.getElementById("input").value = input;
+console.log(program);
 
-wasm.greet();
+init().then(() => {
+    let program, input; 
+  document.getElementById("run").onclick = () =>{
+    document.getElementById("output").innerText = run(
+        program = document.getElementById("program").value,
+        input = document.getElementById("input").value);
+        addr.searchParams.set("program", btoa(program));
+        addr.searchParams.set("input", btoa(input));
+        document.getElementById("link").href = addr;
+    }
+});
